1e977bf884a02b8291dbef1706c84a2d0d02e0ce
Friedrich482 <friedricht109@gmail.com>
2024-04-13 02:03:05 +0200

    successfully added the toast when a house is bought (without prop drilling)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Houses from "./components/House";
 import Footer from "./components/Footer";
-import "./index.css";
-
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
-    <div className="align-center flex flex-col justify-center gap-12">
-      <Header />
-      <main className="flex w-full flex-wrap items-center justify-center gap-10 pl-10 pr-10">
-        <Houses />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <div className="align-center flex flex-col justify-center gap-12">
+        <Header />
+        <main className="flex w-full flex-wrap items-center justify-center gap-10 pl-10 pr-10">
+          <Houses />
+        </main>
+        <Footer />
+      </div>
+      <ToastContainer className="" />
+    </>
   );
 }
 
--- src/components/House/Houses.tsx
+++ src/components/House/Houses.tsx
@@ -1,8 +1,21 @@
 import House from "./House";
 import HousesData from "./HousesData";
-const Houses = () => {
+import type { HousesProps } from "../../types";
+const Houses = ({
+  houseSold,
+  setHouseSold,
+  handleSoldButtonClick,
+}: HousesProps) => {
   return HousesData.map(({ src, id, price, location }) => (
-    <House src={src} id={id} price={price} location={location} />
+    <House
+      src={src}
+      id={id}
+      price={price}
+      location={location}
+      houseSold={houseSold}
+      setHouseSold={setHouseSold}
+      handleSoldButtonClick={handleSoldButtonClick}
+    />
   ));
 };
 
