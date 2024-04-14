41aa703eb93136fc373678d9200787dd5fd9773e
Friedrich482 <friedricht109@gmail.com>
2024-04-14 04:11:52 +0200

    navbar icons click behavior set, problem with the state management for dark Mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,23 @@ import { FaDesktop } from "react-icons/fa";
 import { MdSell } from "react-icons/md";
 import { useState } from "react";
 const Header = () => {
-  const [DropDownList, setDropDownList] = useState(false);
-  const darkModeButtonHandler = () => {};
+  const [dropDownList, setDropDownList] = useState(false);
+  const darkModeButtonHandler = (previousState: boolean) => {
+    setDropDownList(!previousState);
+  };
+  const [dark, setDark] = useState(false);
+  const darkHandler = (previousState: boolean) => {
+    previousState = true;
+    setDark(previousState);
+  };
+  const lightHandler = (previousState: boolean) => {
+    previousState = false;
+    setDark(previousState);
+  };
   return (
-    <header className="mt-6 flex flex-col items-end justify-center gap-2 text-black">
+    <header className="mt-6 flex flex-col items-end justify-center text-black">
       <nav className="flex w-full flex-row items-center justify-start text-xl">
-        <div className="flex w-3/12 items-center justify-start pl-12">
+        <div className="flex w-3/12 flex-row items-center justify-start pl-12">
           <MdSell className="size-6" />
           <p className="cursor-pointer text-black">Friedrich Sell's</p>
         </div>
@@ -19,18 +30,40 @@ const Header = () => {
           <li className="cursor-pointer hover:text-black">Cars</li>
           <li className="cursor-pointer hover:text-black">Motorbikes</li>
         </ul>
-        <div className="mr-8 flex w-2/12 items-center justify-end gap-5">
-          <MdDarkMode />
-          <FaGithub />
+        <div className="mr-8 flex w-2/12 items-center justify-end gap-0">
+          <div className=" flex cursor-pointer items-center justify-center p-3 hover:rounded-lg hover:bg-neutral-300 focus:bg-neutral-400">
+            <MdDarkMode
+              onClick={() => {
+                darkModeButtonHandler(dropDownList);
+              }}
+            />
+          </div>
+          <div className=" flex cursor-pointer items-center justify-center p-3 hover:rounded-lg hover:bg-neutral-300 focus:bg-neutral-400">
+            <FaGithub />
+          </div>
         </div>
       </nav>
-      <div className="flex items-center justify-end backdrop-blur-sm">
-        <ul className="z-10 mr-3 w-32 rounded-lg bg-black p-1 text-base text-white ring-1 ring-stone-800">
-          <li className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1 pl-2 text-start hover:bg-stone-800">
+      <span className="z-10 h-0">
+        <ul
+          className={`mr-3 w-32 flex-col rounded-lg bg-black p-1  text-base text-white ring-1 ring-stone-800 ${dropDownList ? "flex" : "invisible"} `}
+        >
+          <li
+            className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1 pl-2 text-start hover:bg-stone-800"
+            onClick={() => {
+              lightHandler(dark);
+              console.log(dark);
+            }}
+          >
             <CiLight className="text-white" />
             Light
           </li>
-          <li className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1  pl-2 text-start hover:bg-stone-800">
+          <li
+            className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1  pl-2 text-start hover:bg-stone-800"
+            onClick={() => {
+              darkHandler(dark);
+              console.log(dark);
+            }}
+          >
             <MdDarkMode className="" />
             Dark
           </li>
@@ -39,7 +72,7 @@ const Header = () => {
             System
           </li>
         </ul>
-      </div>
+      </span>
     </header>
   );
 };
