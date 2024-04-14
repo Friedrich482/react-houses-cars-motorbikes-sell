9d1cbb7ddf3bf774dbee0157a5fcfa0afe00cca8
Friedrich482 <friedricht109@gmail.com>
2024-04-14 02:31:23 +0200

    added the dropdown for the light/dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
 import { MdDarkMode } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
+import { FaDesktop } from "react-icons/fa";
 import { MdSell } from "react-icons/md";
+import { useState } from "react";
 const Header = () => {
+  const [DropDownList, setDropDownList] = useState(false);
+  const darkModeButtonHandler = () => {};
   return (
-    <header className="mt-3 text-black">
-      <nav className="flex w-full flex-row items-center justify-start text-2xl">
-        <div className="flex w-3/12 items-center justify-start pl-6">
+    <header className="mt-6 flex flex-col items-end justify-center gap-2 text-black">
+      <nav className="flex w-full flex-row items-center justify-start text-xl">
+        <div className="flex w-3/12 items-center justify-start pl-12">
           <MdSell className="size-6" />
           <p className="cursor-pointer text-black">Friedrich Sell's</p>
         </div>
-        <ul className="flex w-7/12 items-center justify-end gap-5 text-slate-500">
+        <ul className="flex w-7/12 items-center justify-end gap-5 text-stone-500">
           <li className="cursor-pointer hover:text-black">Houses</li>
           <li className="cursor-pointer hover:text-black">Cars</li>
           <li className="cursor-pointer hover:text-black">Motorbikes</li>
@@ -19,6 +24,22 @@ const Header = () => {
           <FaGithub />
         </div>
       </nav>
+      <div className="flex items-center justify-end backdrop-blur-sm">
+        <ul className="z-10 mr-3 w-32 rounded-lg bg-black p-1 text-base text-white ring-1 ring-stone-800">
+          <li className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1 pl-2 text-start hover:bg-stone-800">
+            <CiLight className="text-white" />
+            Light
+          </li>
+          <li className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1  pl-2 text-start hover:bg-stone-800">
+            <MdDarkMode className="" />
+            Dark
+          </li>
+          <li className="flex cursor-pointer items-center justify-start gap-2 rounded-lg p-1 pl-2 text-start hover:bg-stone-800">
+            <FaDesktop />
+            System
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
