035337b52bf7f2c0912cb09de4e46b031e691c93
Friedrich482 <friedricht109@gmail.com>
2024-04-19 19:42:50 +0200

    styled the navbar properly to fit dark and light mode

--- src/components/Navbar/MenuBurger.tsx
+++ src/components/Navbar/MenuBurger.tsx
@@ -2,27 +2,64 @@ import type { DarkModeProps } from "../../types";
 import { twMerge as tm } from "tailwind-merge";
 import { IoIosClose } from "react-icons/io";
 import { MdSell } from "react-icons/md";
+import { useState } from "react";
 const MenuBurger = ({ dark }: DarkModeProps) => {
+  const [openVerticalNavbar, setOpenVerticalNavbar] = useState(true);
+  const handleCloseButtonClick = (previousState: boolean) => {
+    setOpenVerticalNavbar(!previousState);
+  };
   return (
-    <span className="z-20 inline h-0 place-self-start">
+    <span
+      className={
+        openVerticalNavbar
+          ? "z-20 inline h-0 transform place-self-start duration-1000"
+          : "hidden"
+      }
+    >
       <div
         className={tm(
-          "flex h-dvh w-[22rem] min-w-48 max-w-[32.4rem] flex-col items-center justify-start border border-stone-600 bg-default-white pt-5 sm:hidden",
-          dark && "text-default-white",
+          "flex h-[200vh] w-[22rem] min-w-48 max-w-[32.4rem] flex-col items-center justify-start gap-8 border border-stone-600 bg-default-white pt-5 sm:hidden",
+          dark && "bg-default-black",
         )}
       >
-        <div className=" flex items-center justify-center gap-2 pr-2">
-          <MdSell className={tm("size-6 text-black", dark && "text-white")} />
-          <p className={tm("cursor-pointer text-black", dark && "text-white")}>
-            Friedrich Sell's
-          </p>
-          <IoIosClose className="align-self-end size-6 cursor-pointer rounded-md border border-black" />
+        <div className="justify- flex w-full items-center justify-between pr-2">
+          <div className="ml-8 flex flex-row items-center justify-center gap-2">
+            <MdSell className={tm("size-6 text-black", dark && "text-white")} />
+            <p
+              className={tm("cursor-pointer text-black", dark && "text-white")}
+            >
+              Friedrich Sell's
+            </p>
+          </div>
+          <IoIosClose
+            className={tm(
+              "hover: mr-3 size-6 cursor-pointer rounded-md border-2 border-stone-500 text-stone-950 hover:border-default-black hover:text-default-black",
+              dark &&
+                "border-stone-400 text-stone-400 hover:border-default-white hover:text-default-white",
+            )}
+            onClick={() => {
+              handleCloseButtonClick(openVerticalNavbar);
+            }}
+          />
         </div>
 
-        <ul className="flex flex-col items-start justify-center">
-          <li>Houses</li>
-          <li>Cars </li>
-          <li>Motorbikes</li>
+        <ul className="flex w-full flex-col gap-3">
+          {" "}
+          <li
+            className={tm("ml-16 cursor-pointer", dark && "text-default-white")}
+          >
+            Houses
+          </li>
+          <li
+            className={tm("ml-16 cursor-pointer", dark && "text-default-white")}
+          >
+            Cars{" "}
+          </li>
+          <li
+            className={tm("ml-16 cursor-pointer", dark && "text-default-white")}
+          >
+            Motorbikes
+          </li>
         </ul>
       </div>
     </span>
