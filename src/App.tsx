10560b128754b7e573e088a81ed9c7a745a6c414
Friedrich482 <friedricht109@gmail.com>
2024-04-23 22:44:40 +0200

    minimal markup and design for the hero section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Houses from "./components/House";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
 import { twMerge as tm } from "tailwind-merge";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
@@ -16,6 +17,7 @@ function App() {
         )}
       >
         <Header dark={dark} setDark={setDark} />
+        <HeroSection />
         <main className="flex w-full flex-wrap items-center justify-center gap-10 pl-10 pr-10">
           <Houses dark={dark} setDark={setDark} />
         </main>
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+  return (
+    <section className="mb-28 flex flex-col items-center justify-center tracking-tighter sm:ml-16 sm:mr-16">
+      <h1 className="mt-12 max-w-[800px] text-center text-4xl font-extrabold sm:mt-20 sm:text-5xl">
+        Discover Your Next Journey: Homes, Cars, and Bikes Await!
+      </h1>
+      <p className=" mt-8 max-w-[700px] text-center text-xl text-neutral-700 sm:text-2xl">
+        Awesome website to buy your cars, homes and bikes and best prices and
+        with only few clicks!
+      </p>
+      <button className="mt-5 rounded-lg bg-black p-3 pl-8 pr-8 text-xl text-default-white">
+        Discover us
+      </button>
+    </section>
+  );
+};
+export default HeroSection;
