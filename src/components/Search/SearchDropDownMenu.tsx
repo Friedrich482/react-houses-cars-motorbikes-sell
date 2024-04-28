c08f482792f65dd69ba02f59401de799f642001a
Friedrich482 <friedricht109@gmail.com>
2024-04-29 01:40:59 +0200

    managed the state to show and hide the dropdown menu

--- src/components/Search/SearchDropDownMenu.tsx
+++ src/components/Search/SearchDropDownMenu.tsx
@@ -1,51 +1,55 @@
 import { useState } from "react";
 import SearchPriceBar from "./SearchPriceBar";
+import DefaultSearchBar from "./DefaultSearchBar";
+import DropDownMenu from "./DropDownMenu";
 import { twMerge as tm } from "tailwind-merge";
 import type { SearchPrice } from "../../types";
-import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 const SearchDropDownMenu = ({
   dark,
   priceSearch,
   setPriceSearch,
 }: SearchPrice) => {
   const [searchParameter, setSearchParameter] = useState("none");
+  const [dropDownMenuVisibility, setDropDownMenuVisibility] = useState(false);
   return (
-    <div
-      className={tm(
-        "group flex h-7 w-7/12 items-center justify-center house-break:w-5/12",
-        dark && "",
-        searchParameter === "none" && "cursor-pointer",
-      )}
-    >
+    <>
       <div
         className={tm(
-          "group flex h-full w-full min-w-40 items-center justify-center rounded-lg border border-neutral-600 bg-default-white house-break:w-6/12",
-          dark && "bg-default-black",
+          "group flex h-7 w-7/12 items-center justify-center house-break:w-5/12",
+          dark && "",
+          searchParameter === "none" && "cursor-pointer",
         )}
       >
-        {searchParameter === "none" ? (
-          <button
-            className={tm(
-              "flex items-center justify-between",
-              dark && "text-white",
-              searchParameter && "w-9/12",
-            )}
-          >
-            <div className="">Select...</div>
-            <div className="flex h-5 flex-col items-start justify-center">
-              <MdKeyboardArrowUp className="size-3" />
-              <MdKeyboardArrowDown className="size-3" />
-            </div>
-          </button>
-        ) : (
-          <SearchPriceBar
-            dark={dark}
-            priceSearch={priceSearch}
-            setPriceSearch={setPriceSearch}
-          />
-        )}
+        <div
+          className={tm(
+            "group flex h-full w-full min-w-40 items-center justify-center rounded-lg border border-neutral-600 bg-default-white house-break:w-6/12",
+            dark && "bg-default-black",
+          )}
+        >
+          {searchParameter === "none" ? (
+            <DefaultSearchBar
+              dark={dark}
+              searchParameter={searchParameter}
+              dropDownMenuVisibility={dropDownMenuVisibility}
+              setDropDownMenuVisibility={setDropDownMenuVisibility}
+            />
+          ) : (
+            <SearchPriceBar
+              dark={dark}
+              priceSearch={priceSearch}
+              setPriceSearch={setPriceSearch}
+            />
+          )}
+        </div>
       </div>
-    </div>
+
+      {dropDownMenuVisibility ? (
+        <DropDownMenu
+          searchParameter={searchParameter}
+          setSearchParameter={setSearchParameter}
+        />
+      ) : null}
+    </>
   );
 };
 export default SearchDropDownMenu;
